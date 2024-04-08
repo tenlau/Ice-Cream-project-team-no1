7e902188ebe9cb7385ff61c132d526c03c7b2567
bardansimona123 <bardan.simona2006@gmail.com>
2024-04-08 15:56:07 +0100

    finalizare css

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+// Functionalitatea pentru Sectiunea 2- Products //
+
+function showDescription(button) {
+    var descriereCompleta = button.getAttribute("data-descriere-completa");
+    var parinteDiv = button.parentNode;
+    var initialDescription = parinteDiv.querySelector(" .initial-description");
+
+    initialDescription.textContent += descriereCompleta;
+
+    button.style.display = "none";
+
+    var backButton = document.createElement("button");
+    backButton.textContent = "Show less";
+    backButton.classList.add("back-btn");
+    backButton.onclick = function() {
+        initialDescription.textContent = initialDescription.textContent.replace(descriereCompleta, "");
+        button.style.display = "block";
+        backButton.remove();
+    }
+
+    parinteDiv.appendChild(backButton);
+}
