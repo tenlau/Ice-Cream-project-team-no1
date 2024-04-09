5006d5073e0e963b978586d5f6a65f1dad21fd45
Loredana-Lungu <lunguioanaloredana@gmail.com>
2024-04-09 16:51:11 +0300

    update-3rd attempt
    adding of modern normalize file

--- src/sass/reviews.js
+++ src/sass/reviews.js
@@ -20,8 +20,10 @@ function showSlides(n) {
       slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace("active", "");
     }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += "active";
 }
+
+
